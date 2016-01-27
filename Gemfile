5065c367a58dd33058997b115e4106ea43daeb8b
thomasjacobjr <thomasjacobjr@gmail.com>
2016-01-27 15:09:17 -0700

    Checkpoint 36 Rails - Authentication User Model: Completed

--- Gemfile
+++ Gemfile
@@ -61,3 +61,5 @@ group :development do
 end
 
 gem 'bootstrap-sass'
+
+gem 'bcrypt'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127030739) do
+ActiveRecord::Schema.define(version: 20160127214326) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 20160127030739) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
