750f5600248ca023d07f1b874e3deb50f66a1f8c
thomasjacobjr <thomasjacobjr@gmail.com>
2016-01-28 15:58:36 -0700

    Checkpoint 41 Rails - Comments: Completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,19 +2,20 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
-  it { should have_many(:posts)}
-  # Shoulda tests for name
+
+  it { should have_many(:posts) }
+  it { should have_many(:comments) }
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(1) }
 
-  # Shoulda tests for email
+
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_length_of(:email).is_at_least(3) }
   it { should allow_value("user@bloccit.com").for(:email) }
   it { should_not allow_value("userbloccit.com").for(:email) }
 
-  # Shoulda tests for password
+
   it { should validate_presence_of(:password) }
   it { should have_secure_password }
   it { should validate_length_of(:password).is_at_least(6) }
